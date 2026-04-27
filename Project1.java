52c64d7123406cd8cc62ac4786a96188439abdca
yashika <yashika0432@gmail.com>
2026-04-27 19:56:22 +0530

    first project

---
+++ Project1.java
@@ -0,0 +1,13 @@
+public class Project1{
+    public static void main(String[] args){
+        String sentence="I will never spam my friends again.";
+    
+
+    for(int i=0;i<=100;i++){
+        if(i%10==0){
+            System.out.println(i + ". I will never spaM my friendz again.");
+        }else{
+            System.out.println(i+ ". "+sentence);
+        }
+    }}
+}
